973feeba6db3351d4314d90a6ef2cb23f35de395
Simon Robinson <simon@robinson.ac>
2013-04-08 13:16:08 +0100

    Convert a drawable to a bitmap

--- src/ac/robinson/util/BitmapUtilities.java
+++ src/ac/robinson/util/BitmapUtilities.java
@@ -40,6 +40,8 @@ import android.graphics.Paint.Align;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.YuvImage;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.media.ExifInterface;
 import android.text.TextUtils;
 
@@ -533,6 +535,24 @@ public class BitmapUtilities {
 		} finally {
 			IOUtilities.closeStream(bufferedOutputStream);
 		}
+	}
+
+	public static Bitmap drawableToBitmap(Drawable drawable) {
+		if (drawable instanceof BitmapDrawable) {
+			return ((BitmapDrawable) drawable).getBitmap();
+		}
+
+		int width = drawable.getIntrinsicWidth();
+		width = width > 0 ? width : 1;
+		int height = drawable.getIntrinsicHeight();
+		height = height > 0 ? height : 1;
+
+		Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+		Canvas canvas = new Canvas(bitmap);
+		drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+		drawable.draw(canvas);
+
+		return bitmap;
 	}
 
 	/**
